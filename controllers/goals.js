a2f0c8f3eb44d1cceba669b9569bbc967eec3e8b
mikehoods <62457440+mikehoods@users.noreply.github.com>
2020-08-03 16:27:27 -0500

    setup initial goal model and controllers

---
+++ controllers/goals.js
@@ -0,0 +1,67 @@
+///////////DEPENDENCIES//////////
+
+const express = require('express')
+const router = express.Router()
+const Goal = require('../models/goals.js')
+
+///////////ROUTES///////////
+
+/////Create Route:
+
+router.post('/', async (req, res) => {
+    try {
+        const newGoal = Goal.create(req.body.goal)
+        res.status(200).json(newGoal)
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
+
+/////Read Route:
+
+router.get('/', async (req, res) => {
+    try {
+        const goals = Goal.find({}, (errors, goals) => {
+            res.status(200).json(goals)
+        })
+        return goals
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
+
+/////Show Route:
+
+router.get('/:id', async (req, res) => {
+    try {
+        const goal = await Goal.findById(req.params.id)
+        res.status(200).json(goal)
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
+
+/////Delete Route:
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
+        res.status(200).json(deletedGoal)
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
+
+/////Update Route:
+
+router.put('/:id', async (req, res) => {
+    try {
+        const updatedGoal = await Goal.findByIdAndUpdate(
+            req.params,id,
+            req.body.goal
+        )
+        res.status(200).json(updatedGoal)
+    } catch(error) {
+        res.status(400).json(error)
+    }
+})
---
+++ models/goals.js
@@ -0,0 +1,17 @@
+const {Schema, model} = require ('mongoose')
+
+const goalSchema = new Schema({
+    name: String,
+    description: String,
+    category: String,
+    difficulty: Number,
+    importance: Number,
+    progress: Number,
+    complete: Boolean,
+    finishBy: Date,
+    notes: String
+})
+
+const Goal = model('Goal', goalSchema)
+
+module.exports = Goal
