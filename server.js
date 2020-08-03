dd67f529287f274f5dbdeb7b46f3560337c6cda9
mikehoods <62457440+mikehoods@users.noreply.github.com>
2020-08-03 14:28:27 -0500

    setup server.js

---
+++ server.js
@@ -0,0 +1,39 @@
+//////////DEPENDENCIES//////////
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+require('dotenv').config()
+
+//////////GLOBALS//////////
+
+const PORT = process.env.PORT || 3000
+const goalsController = require('./controllers/goals.js')
+const db = mongoose.connection
+const DB_URI = process.env.DB_URI || 'mongodb://localhost:27017'
+
+//////////DATABASE CONNECT//////////
+
+mongoose.connect(DB_URI, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+db.on('open', ()=> {
+    console.log('Totally connected to Mongo.')
+})
+
+//////////MIDDLEWARE//////////
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+app.use('/goals/', goalsController)
+
+//////////LISTENER//////////
+app.listen(PORT, ()=> {
+    console.log(`Thank you for listening to Port: ${PORT}`)
+})
