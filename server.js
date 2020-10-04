9dac09a9a8aa4ac1b63b9a4bb562ed33b6eb2ea4
mikehoods <62457440+mikehoods@users.noreply.github.com>
2020-10-04 16:43:49 -0500

    removed users model and controller. switched to 3rd party auth.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 
 require('dotenv').config()
 
@@ -13,8 +12,6 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 4000
 const goalsController = require('./controllers/goals.js')
-const usersController = require('./controllers/users.js')
-const db = mongoose.connection
 const MONGODB_URI = process.env.DB_URI
 
 //allow cross-origin requests
@@ -25,7 +22,7 @@ const MONGODB_URI = process.env.DB_URI
 //allow cross-origin requests
 app.use(cors());
 
-//connect to mlab (Atlas) database
+//////////DATABASE CONNECT//////////
 mongoose.connect('mongodb+srv://PlantManATX:plants@cluster0.emvle.mongodb.net/goalsDB?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -35,24 +32,12 @@ mongoose.connect('mongodb+srv://PlantManATX:plants@cluster0.emvle.mongodb.net/go
 .then(()=> console.log('Connected to MongoDB Atlas'))
 .catch(err => console.log('Error: ', err.message));
 
-//////////DATABASE CONNECT//////////
-
-// mongoose.connect(MONGODB_URI, {
-//     useCreateIndex: true,
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true
-// })
-// db.on('open', ()=> {
-//     console.log('Totally connected to Mongo.')
-// })
-
 //////////MIDDLEWARE//////////
 
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 app.use('/goals/', goalsController)
-app.use('/users/', usersController)
 
 //////////LISTENER//////////
 app.listen(PORT, ()=> {
